6a330257b54de6a6c69120683e5e9ea338b1262c
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-02 20:01:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,10 +8,10 @@ def line(katz_deli)
     line = ""
     count = 0
     while count < katz_deli.length
-      line << "#{count+1}. #{katz_deli[count} "
+      line << "#{count+1}. #{katz_deli[count]} "
       count +=1
     end
-    puts "The line is currently: #{line}"  
+    puts "The line is currently: #{line}"
   end
 end
 
